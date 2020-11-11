45b7686076a89e4948ea0be80a789a1a65ea3c5f
sincosab <1132225715@qq.com>
2020-11-11 14:51:41 +0800

    修改https抓取

--- src/main/java/cn/edu/hfut/dmic/webcollector/plugin/net/OkHttpRequester.java
+++ src/main/java/cn/edu/hfut/dmic/webcollector/plugin/net/OkHttpRequester.java
@@ -25,8 +25,18 @@ import okhttp3.*;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.HashSet;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * @author hu
@@ -58,13 +68,43 @@ public class OkHttpRequester extends DefaultConfigured implements Requester{
     public OkHttpClient.Builder createOkHttpClientBuilder(){
         OkHttpClient.Builder builder = new OkHttpClient.Builder()
                 .followRedirects(false)
+                .sslSocketFactory(createSSLSocketFactory())  
+                .hostnameVerifier(new TrustAllHostnameVerifier())  
                 .followSslRedirects(false)
                 .connectTimeout(getConf().getConnectTimeout(), TimeUnit.MILLISECONDS)
                 .readTimeout(getConf().getReadTimeout(), TimeUnit.MILLISECONDS);
         return builder;
 
     }
-
+    private static class TrustAllCerts implements X509TrustManager {  
+        @Override  
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}  
+   
+        @Override  
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}  
+   
+        @Override  
+        public X509Certificate[] getAcceptedIssuers() {return new X509Certificate[0];}  
+    }  
+    private static class TrustAllHostnameVerifier implements HostnameVerifier {  
+        @Override  
+        public boolean verify(String hostname, SSLSession session) {  
+            return true;  
+        }  
+    }  
+    private static SSLSocketFactory createSSLSocketFactory() {  
+        SSLSocketFactory ssfFactory = null;  
+   
+        try {  
+            SSLContext sc = SSLContext.getInstance("TLS");  
+            sc.init(null,  new TrustManager[] { new TrustAllCerts() }, new SecureRandom());  
+   
+            ssfFactory = sc.getSocketFactory();  
+        } catch (Exception e) {  
+        }  
+   
+        return ssfFactory;  
+    } 
     public Request.Builder createRequestBuilder(CrawlDatum crawlDatum){
         Request.Builder builder = new Request.Builder()
                 .header("User-Agent",getConf().getDefaultUserAgent())
